f220a699abe255e3c3d287f959068dbeff9c2161
yushusongmsazure <yushusong@outlook.com>
2019-04-12 17:49:58 -0700

    everything works except getting donor name and value from session

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,7 +15,30 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
+
+@app.route('/create', methods=['GET','POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('create.jinja2')
     
+    if request.method == 'POST':
+        donor = Donor.select().where(Donor.name == 'Charlie').get()
+        print(donor)
+        old_donation = Donation.select().where(Donation.donor == donor).get()
+        print(old_donation)
+
+        # Donation.update(performed=, performed_by=user)\
+        #     .where(Task.id == request.form['task_id'])\
+        #     .execute()
+        #user = User.select().where(User.name == session['username']).get()
+
+        # Donation.update(performed=datetime.now(), performed_by=user)\
+        #     .where(Task.id == request.form['task_id'])\
+        #     .execute()
+        new_donation = Donation(value=10, donor=donor)
+        new_donation.save()
+        return redirect(url_for('home'))
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
